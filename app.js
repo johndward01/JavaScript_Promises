940676217f6f4e21db893e0405942ff58304bba3
johndward01 <jackwade464@gmail.com>
2021-05-25 15:27:54 -0500

    finised exercise 1

--- app.js
+++ app.js
@@ -2,3 +2,18 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+let watching = new Promise((resolve, reject) => {
+    let userWatchingLiveStream;
+    if (userWatchingLiveStream) {
+        resolve("Thumbs up and Subscirbe!")
+    }
+    else {
+        reject("User left")
+    }
+});
+
+watching.then((message) => {
+    console.log(`Message: ${message}`);
+}).catch((message) => {
+    console.log(`Error Message: ${message}`);
+})
